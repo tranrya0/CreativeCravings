9dd8aee02d89abdb9e949518240a816d1b7f5797
tranrya0 <tranrya0@uw.edu>
2018-02-16 18:26:05 -0800

    added basic user roles for admin and moderator roles

--- CreativeCravings/Startup.cs
+++ CreativeCravings/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using CreativeCravings.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(CreativeCravings.Startup))]
@@ -9,6 +12,59 @@ namespace CreativeCravings
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createUserRoles();
+        }
+
+        // create user roles on startup if they do not already exist
+        // code originally from https://code.msdn.microsoft.com/ASPNET-MVC-5-Security-And-44cbdb97
+        private void createUserRoles()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            // In Startup iam creating first Admin Role and creating a default Admin User    
+            if (!roleManager.RoleExists("Admin"))
+            {
+
+                // first we create Admin role
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                /**
+                
+                ----------- Create user with admin role here -----------------------
+
+
+                //Here we create a Admin super user who will maintain the website                  
+                var user = new ApplicationUser();
+                user.UserName = "shanu";
+                user.Email = "admin@gmail.com";
+
+                string userPWD = "adminpassword";
+
+                var chkUser = UserManager.Create(user, userPWD);
+
+                //Add default User to Role Admin   
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+
+                }
+                
+             */
+            }
+
+            // creating Creating Manager role    
+            if (!roleManager.RoleExists("Moderator"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Manager";
+                roleManager.Create(role);
+
+            }
         }
     }
 }
